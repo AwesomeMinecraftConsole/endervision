fa1cac32fbd11c1282f739f4d1bdef82e9eba584
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-02 20:39:41 +0900

    :recycle: gradle.propertiesから参照

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -20,13 +20,14 @@ val grpc_version: String by project
 val grpc_kotlin_version: String by project
 
 dependencies {
+    val coroutines_version: String by project
     val kodein_version: String by project
 
     implementation(project(":common"))
     implementation(compose.desktop.currentOs)
     implementation("org.kodein.di:kodein-di:${kodein_version}")
     implementation("org.kodein.di:kodein-di-framework-compose:${kodein_version}")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
     implementation("com.google.protobuf:protobuf-java-util:$protobuf_version")
     implementation("io.grpc:grpc-netty-shaded:$grpc_version")
     implementation("io.grpc:grpc-protobuf:$grpc_version")
