602486362f0838a3b545d88bac3cbcfba554e417
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-04 20:52:40 +0900

    :recycle: Lineを別ファイルで定義

--- common/src/commonMain/kotlin/com/uramnoil/minecraft_awesome_console/endervision/common/models/usecase/Line.kt
+++ common/src/commonMain/kotlin/com/uramnoil/minecraft_awesome_console/endervision/common/models/usecase/Line.kt
@@ -1,5 +1,3 @@
 package com.uramnoil.minecraft_awesome_console.endervision.common.models.usecase
 
-import kotlinx.coroutines.flow.Flow
-
-class LineViewModel(val lineFlow: Flow<Line>)
+data class Line(val line: String)
