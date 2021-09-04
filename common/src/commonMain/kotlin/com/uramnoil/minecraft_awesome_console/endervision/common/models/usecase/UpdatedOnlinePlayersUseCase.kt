b02fa80871d2fcaaa80849905e6cfa84489ebc0a
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-04 17:58:10 +0900

    :fire: OnlinePlayer

--- common/src/commonMain/kotlin/com/uramnoil/minecraft_awesome_console/endervision/common/models/usecase/UpdatedOnlinePlayersUseCase.kt
+++ common/src/commonMain/kotlin/com/uramnoil/minecraft_awesome_console/endervision/common/models/usecase/UpdatedOnlinePlayersUseCase.kt
@@ -1,7 +1,5 @@
 package com.uramnoil.minecraft_awesome_console.endervision.common.models.usecase
 
-data class OnlinePlayer(val id: String, val name: String, val ping: Int)
-
 interface UpdatedOnlinePlayersUseCaseInputPort {
     fun execute(onlinePlayers: List<OnlinePlayer>)
 }
