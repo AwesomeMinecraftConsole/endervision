466ea8d48ff7d9c5cfe01aaa94d5ddd9bd01d104
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-05 14:54:55 +0900

    :bug: OnlinePlayers -> OnlinePlayer

--- common/src/commonMain/kotlin/com/uramnoil/minecraft_awesome_console/endervision/common/models/usecase/OnlinePlayer.kt
+++ common/src/commonMain/kotlin/com/uramnoil/minecraft_awesome_console/endervision/common/models/usecase/OnlinePlayer.kt
@@ -6,4 +6,4 @@ data class Ping(val value: UInt)
 
 data class OnlinePlayer(val id: Id, val name: Name, val ping: Ping)
 
-data class OnlinePlayers(val value: List<OnlinePlayers>)
+data class OnlinePlayers(val value: List<OnlinePlayer>)
