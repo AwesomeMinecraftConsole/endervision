36f15ba7b6ecd0cbaeab1555f6605c4fc53a198d
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-05 16:33:27 +0900

    :sparkles: SendCommandController

---
+++ desktop/src/main/kotlin/com/uramnoil/awesome_minecraft_console/endervision/presentation/SendCommandController.kt
@@ -0,0 +1,10 @@
+package com.uramnoil.awesome_minecraft_console.endervision.presentation
+
+import com.uramnoil.minecraft_awesome_console.endervision.common.models.usecase.Command
+import com.uramnoil.minecraft_awesome_console.endervision.common.models.usecase.SendCommandUseCaseInputPort
+
+class SendCommandController(private val inputPort: SendCommandUseCaseInputPort) {
+    fun sendCommand(command: String) {
+        inputPort.execute(Command(command))
+    }
+}
