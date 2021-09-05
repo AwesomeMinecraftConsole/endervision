bfd15307b3656caf9419b17fb854f3b8a64f4f8e
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-05 14:45:32 +0900

    :sparkles: SendCommandUseCaseInteractor

--- desktop/src/main/kotlin/com/uramnoil/awesome_minecraft_console/endervision/infrastructure/SendCommandUseCaseInteractor.kt
+++ desktop/src/main/kotlin/com/uramnoil/awesome_minecraft_console/endervision/infrastructure/SendCommandUseCaseInteractor.kt
@@ -1,10 +1,11 @@
 package com.uramnoil.awesome_minecraft_console.endervision.infrastructure
 
+import com.uramnoil.minecraft_awesome_console.endervision.common.models.usecase.Command
 import com.uramnoil.minecraft_awesome_console.endervision.common.models.usecase.EnderVisionService
 import com.uramnoil.minecraft_awesome_console.endervision.common.models.usecase.SendCommandUseCaseInputPort
 
 class SendCommandUseCaseInteractor(val service: EnderVisionService) : SendCommandUseCaseInputPort {
-    override fun execute(command: String) {
+    override fun execute(command: Command) {
         service.sendCommand(command)
     }
 }
