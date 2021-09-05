622f7605912f1c28283ba792639e7b618da4c087
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-05 16:54:03 +0900

    :sparkles: OnlinePlayersViewModel

---
+++ common/src/commonMain/kotlin/com/uramnoil/awesome_minecraft_console/endervision/common/presentation/OnlinePlayersViewModel.kt
@@ -0,0 +1,6 @@
+package com.uramnoil.awesome_minecraft_console.endervision.common.presentation
+
+import com.uramnoil.awesome_minecraft_console.endervision.common.usecase.OnlinePlayers
+import kotlinx.coroutines.flow.Flow
+
+data class OnlinePlayersViewModel(val onlinePlayersFlow: Flow<OnlinePlayers>)
