bf48ff8d16969e89f0617155cf451028a043898e
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-06 18:29:05 +0900

    :sparkles: connect, disconnect

--- common/src/commonMain/kotlin/com/uramnoil/awesome_minecraft_console/endervision/common/usecase/EnderVisionService.kt
+++ common/src/commonMain/kotlin/com/uramnoil/awesome_minecraft_console/endervision/common/usecase/EnderVisionService.kt
@@ -1,6 +1,8 @@
 package com.uramnoil.awesome_minecraft_console.endervision.common.usecase
 
 interface EnderVisionService {
+    fun connect()
+    fun disconnect()
     fun sendCommand(command: Command)
     fun sendOperation(operation: Operation)
 }
--- desktop/src/main/kotlin/com/uramnoil/awesome_minecraft_console/endervision/grpc/EnderVisionServiceImpl.kt
+++ desktop/src/main/kotlin/com/uramnoil/awesome_minecraft_console/endervision/grpc/EnderVisionServiceImpl.kt
@@ -1,66 +1,85 @@
 package com.uramnoil.awesome_minecraft_console.endervision.grpc
 
-import com.uramnoil.awesome_minecraft_console.endervision.common.usecase.Operation.*
 import com.uramnoil.awesome_minecraft_console.endervision.common.usecase.*
-import io.grpc.ManagedChannel
+import com.uramnoil.awesome_minecraft_console.endervision.common.usecase.OnlinePlayer
+import com.uramnoil.awesome_minecraft_console.endervision.common.usecase.Operation.Start
+import io.grpc.ManagedChannelBuilder
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
-class EnderVision(
+class EnderVisionServiceImpl(
     private val newLineUseCaseInputPort: NewLineUseCaseInputPort,
     private val sendNotificationUseCaseInputPort: SendNotificationUseCaseInputPort,
     private val updateOnlinePlayersUseCaseInputPort: UpdateOnlinePlayersUseCaseInputPort,
-    channel: ManagedChannel
+    private val channel: ManagedChannelBuilder<*>
 ) :
     CoroutineScope, EnderVisionService {
-    private val job = Job()
+    private val job = SupervisorJob()
     override val coroutineContext: CoroutineContext
         get() = job
+
+    private lateinit var flowJob: Job
 
     private val mutableLineFlow = MutableSharedFlow<String>()
     private val mutableCommandFlow = MutableSharedFlow<String>()
     private val mutableNotificationFlow = MutableSharedFlow<String>()
     private val mutableOperationFlow = MutableSharedFlow<Operation>()
-    private val mutableOnlinePlayersFlow = MutableSharedFlow<List<OnlinePlayer>>()
+    private val mutableOnlinePlayersFlow = MutableSharedFlow<List<com.uramnoil.awesome_minecraft_console.endervision.grpc.OnlinePlayer>>()
 
-    private val client: EnderVisionClient = EnderVisionClientImpl(
-        channel = channel,
-        mutableLineFlow = mutableLineFlow,
-        commandFlow = mutableCommandFlow,
-        mutableNotificationFlow = mutableNotificationFlow,
-        operationFlow = mutableOperationFlow,
-        mutableOnlinePlayersFlow = mutableOnlinePlayersFlow,
-        coroutineContext
-    )
+    private var client: EnderVisionClient? = null
 
-    fun start() {
-        client.apply {
+    private var _isConnecting = false
+    val isConnecting: Boolean
+        get() = _isConnecting
+
+    private fun launchFlow() = launch {
+        mutableLineFlow.collect {
+            newLineUseCaseInputPort.execute(it)
+        }
+        mutableNotificationFlow.collect {
+            sendNotificationUseCaseInputPort.execute(Notification(it))
+        }
+        mutableOnlinePlayersFlow.collect {
+            updateOnlinePlayersUseCaseInputPort.execute(OnlinePlayers(it.map { onlinePlayer ->
+                OnlinePlayer(
+                    Id(onlinePlayer.id),
+                    Name(onlinePlayer.name),
+                    Ping(onlinePlayer.ping.toUInt())
+                )
+            }))
+        }
+    }
+
+    override fun connect() {
+        if (!isConnecting) throw IllegalAccessError("Client is now running")
+
+        flowJob = launchFlow()
+
+        client = EnderVisionClientImpl(
+            channel = channel.build(),
+            mutableLineFlow = mutableLineFlow,
+            commandFlow = mutableCommandFlow,
+            mutableNotificationFlow = mutableNotificationFlow,
+            operationFlow = mutableOperationFlow,
+            mutableOnlinePlayersFlow = mutableOnlinePlayersFlow,
+            coroutineContext
+        )
+
+        client?.apply {
             connectConsole()
             connectManagement()
             connectOnlinePlayers()
         }
+    }
 
-        launch {
-            mutableLineFlow.collect {
-                newLineUseCaseInputPort.execute(it)
-            }
-            mutableNotificationFlow.collect {
-                sendNotificationUseCaseInputPort.execute(Notification(it))
-            }
-            mutableOnlinePlayersFlow.collect {
-                updateOnlinePlayersUseCaseInputPort.execute(OnlinePlayers(it.map { onlinePlayer ->
-                    OnlinePlayer(
-                        Id(onlinePlayer.id),
-                        Name(onlinePlayer.name),
-                        Ping(onlinePlayer.ping.toUInt())
-                    )
-                }))
-            }
-        }
+    override fun disconnect() {
+        client?.close()
+        flowJob.cancel()
     }
 
     override fun sendCommand(command: Command) {
@@ -85,7 +104,8 @@ class EnderVisionPresenter(
     private val mutableOperationFlow: MutableSharedFlow<Notification>,
     private val mutableOnlinePlayersFlow: MutableSharedFlow<OnlinePlayers>,
     coroutineContext: CoroutineContext
-) : NewLineUseCaseOutputPort, SendNotificationUseCaseOutputPort, UpdateOnlinePlayersUseCaseOutputPort, CoroutineScope by CoroutineScope(coroutineContext) {
+) : NewLineUseCaseOutputPort, SendNotificationUseCaseOutputPort, UpdateOnlinePlayersUseCaseOutputPort,
+    CoroutineScope by CoroutineScope(coroutineContext) {
     override fun handle(line: Line) {
         launch {
             mutableLineSharedFlow.emit(line)
