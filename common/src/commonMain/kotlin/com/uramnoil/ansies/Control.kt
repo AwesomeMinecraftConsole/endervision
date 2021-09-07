b5d4a1c830d98e7c65a18477d97fb25d48954354
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-08 02:06:03 +0900

    :sparkles: Null, VerticalTab, ControlZ

--- common/src/commonMain/kotlin/com/uramnoil/ansies/Control.kt
+++ common/src/commonMain/kotlin/com/uramnoil/ansies/Control.kt
@@ -1,61 +1,17 @@
 package com.uramnoil.ansies
 
 enum class ControlCharacter(val abbr: String, val c0: Int) {
-    /**
-     * Bell
-     * Makes an audible noise.
-     */
+    Null("Nul", 0x00),
     Bell("BEL", 0x07),
-
-    /**
-     * Backspace
-     * Moves the cursor left (but may "backwards wrap" if cursor is at start of line).
-     */
     Backspace("BS", 0x08),
-
-    /**
-     * Tab
-     * Moves the cursor right to next multiple of 8.
-     */
-    Tab("HT", 0x09),
-
-    /**
-     * Line Feed
-     * Moves to next line, scrolls the display up if at bottom of the screen.
-     * Usually does not move horizontally, though programs should not rely on this.
-     */
+    HorizontalTab("HT", 0x09),
     LineFeed("LF", 0x0A),
-
-    /**
-     *
-     */
-    VT("VT", 0x0B),
-
-    /**
-     * Form Feed
-     * Move a printer to top of next page.
-     * Usually does not move horizontally, though programs should not rely on this.
-     * Effect on video terminals varies.
-     */
+    VerticalTab("VT", 0x0B),
     FormFeed("FF", 0x0C),
-
-    /**
-     * Carriage Return
-     * Moves the cursor to column zero.
-     */
     CarriageReturn("CR", 0x0D),
-
-    /**
-     * Escape
-     * Starts all the escape sequences.
-     */
+    ControlZ("SUB", 0x1A),
     Escape("ESC", 0x1B),
-
-    /**
-     *
-     */
     DEL("DEL", 0x7F), ;
-
     override fun toString(): String = Char(c0).toString()
 }
 
