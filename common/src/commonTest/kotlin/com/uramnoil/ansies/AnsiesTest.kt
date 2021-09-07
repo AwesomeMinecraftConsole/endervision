1da71207912da9e6781d0491818e7f1ae1b1dc70
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-08 02:20:14 +0900

    :bug: 正しい色を使う

--- common/src/commonTest/kotlin/com/uramnoil/ansies/AnsiesTest.kt
+++ common/src/commonTest/kotlin/com/uramnoil/ansies/AnsiesTest.kt
@@ -5,10 +5,66 @@ import kotlin.test.assertEquals
 
 class AnsiesTest {
     @Test
-    fun test() {
+    fun testBlack() {
+        assertEquals(
+            "${Char(0x1B)}${Char(0x9B)}30m",
+            "".black().toString()
+        )
+    }
+
+    @Test
+    fun testRed() {
         assertEquals(
             "${Char(0x1B)}${Char(0x9B)}31m",
             "".red().toString()
+        )
+    }
+
+    @Test
+    fun testGreen() {
+        assertEquals(
+            "${Char(0x1B)}${Char(0x9B)}32m",
+            "".green().toString()
+        )
+    }
+
+    @Test
+    fun testYellow() {
+        assertEquals(
+            "${Char(0x1B)}${Char(0x9B)}33m",
+            "".yellow().toString()
+        )
+    }
+
+    @Test
+    fun testBlue() {
+        assertEquals(
+            "${Char(0x1B)}${Char(0x9B)}34m",
+            "".blue().toString()
+        )
+    }
+
+    @Test
+    fun testMagenta() {
+        assertEquals(
+            "${Char(0x1B)}${Char(0x9B)}35m",
+            "".magenta().toString()
+        )
+    }
+
+    @Test
+    fun testCyan() {
+        assertEquals(
+            "${Char(0x1B)}${Char(0x9B)}36m",
+            "".cyan().toString()
+        )
+    }
+
+    @Test
+    fun testWhite() {
+        assertEquals(
+            "${Char(0x1B)}${Char(0x9B)}37m",
+            "".white().toString()
         )
     }
 }
