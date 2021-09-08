74e5aa151380a144bd64af3b57b18d073768e91e
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-08 16:12:09 +0900

    :art: CSIのc1を0x58に変更

--- common/src/commonMain/kotlin/com/uramnoil/ansies/EscapeSequence.kt
+++ common/src/commonMain/kotlin/com/uramnoil/ansies/EscapeSequence.kt
@@ -4,7 +4,7 @@ enum class EscapeSequenceType(val abbr: String, val c1: Int) {
     SingleShiftTwo("SS2", 0x8E),
     SingleShiftThree("SS3", 0x8F),
     DeviceControlString("DCS", 0x90),
-    ControlSequenceIntroducer("CSI", 0x9B),
+    ControlSequenceIntroducer("CSI", 0x5B),
     StringTerminator("ST", 0x9C),
     OperatingSystemCommand("OSC", 0x9D),
     StartOfString("SOS", 0x98),
