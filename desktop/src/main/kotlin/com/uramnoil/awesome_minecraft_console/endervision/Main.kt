3f29ce156f1b7b83e84923ac692b608c9d0531b7
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-08 16:46:23 +0900

    :art: 整形

--- desktop/src/main/kotlin/com/uramnoil/awesome_minecraft_console/endervision/Main.kt
+++ desktop/src/main/kotlin/com/uramnoil/awesome_minecraft_console/endervision/Main.kt
@@ -1,17 +1,4 @@
-import com.uramnoil.awesome_minecraft_console.endervision.common.presentation.LineViewModel
-import com.uramnoil.awesome_minecraft_console.endervision.common.presentation.NotificationViewModel
-import com.uramnoil.awesome_minecraft_console.endervision.common.presentation.OnlinePlayersViewModel
-import com.uramnoil.awesome_minecraft_console.endervision.common.usecase.*
 import com.uramnoil.awesome_minecraft_console.endervision.compose.app
-import com.uramnoil.awesome_minecraft_console.endervision.compose.viewmodel.CommandViewModel
-import com.uramnoil.awesome_minecraft_console.endervision.infrastructure.EnderVisionServiceImpl
-import com.uramnoil.awesome_minecraft_console.endervision.infrastructure.*
-import com.uramnoil.awesome_minecraft_console.endervision.presentation.CommandController
-import com.uramnoil.awesome_minecraft_console.endervision.presentation.OperationController
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.MutableSharedFlow
-import org.kodein.di.DI
-import org.kodein.di.bindSingleton
 
 suspend fun main() {
     app()
