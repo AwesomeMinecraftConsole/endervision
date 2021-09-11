b040b559f5754def7306f7b8daf8e4d354d4f04c
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-12 08:46:34 +0900

    :sparkle: 要素追加

--- common/src/commonMain/kotlin/com/uramnoil/ansies/EscapeSequence.kt
+++ common/src/commonMain/kotlin/com/uramnoil/ansies/EscapeSequence.kt
@@ -1,15 +1,38 @@
 package com.uramnoil.ansies
 
-enum class EscapeSequenceParameterType(val abbr: String, val c1: Int) {
-    SingleShiftTwo("SS2", 0x8E),
-    SingleShiftThree("SS3", 0x8F),
-    DeviceControlString("DCS", 0x90),
-    ControlSequenceIntroducer("CSI", 0x5B),
-    StringTerminator("ST", 0x9C),
-    OperatingSystemCommand("OSC", 0x9D),
-    StartOfString("SOS", 0x98),
-    PrivacyMessage("PM", 0x9E),
-    ApplicationProgramCommand("APC", 0x9F), ;
+enum class EscapeSequenceParameterType(val abbr: String, val gl: Int, val c1: Int) {
+    PaddingCharacter("PAD", 64, 128),
+    HighOctetPreset("HOP", 65, 129),
+    BreakPermittedHere("BPH", 66, 130),
+    NoBreakHere("NBH", 67, 131),
+    Index("IND", 68, 132),
+    NExtLine("NEL", 69, 133),
+    StartOfSelectedArea("SSA", 70, 134),
+    EndOfSelectedArea("ESA", 71, 135),
+    HorizontalTabulationSet("HTS", 72, 136),
+    HorizontalTabulationWithJustification("HTJ", 73, 137),
+    VerticalTabulationSet("VTS", 74, 138),
+    PartialLineDown("PLD", 75, 139),
+    PartialLineUp("PLU", 76, 140),
+    ReverseIndex("RI", 77, 141),
+    SingleShift2("SS2", 78, 142),
+    SingleShift3("SS3", 79, 143),
+    DeviceControlString("DCS", 80, 144),
+    PrivateUse1("PU1", 81, 145),
+    PrivateUse2("PU2", 82, 146),
+    SetTransmitState("STS", 83, 147),
+    CancelCharacter("CCH", 84, 148),
+    MessageWaiting("MW", 85, 149),
+    StartOfProtectedArea("SPA", 86, 150),
+    EndOfProtectedArea("EPA", 87, 151),
+    StartOfString("SOS", 88, 152),
+    SingleGraphicCharacterIntroducer("SGCI", 89, 153),
+    SingleCharacterIntroducer("SCI", 90, 154),
+    ControlSequenceIntroducer("CSI", 91, 155),
+    StringTerminator("ST", 92, 156),
+    OperatingSystemCommand("OSC", 93, 157),
+    PrivacyMessage("PM", 94, 158),
+    ApplicationProgramCommand("APC", 95, 159), ;
 
     override fun toString(): String = Char(c1).toString()
 }
