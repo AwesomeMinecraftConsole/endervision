9fec190d31db15061228b36c5ea9e47b853a7ee9
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-09-20 17:16:32 +0900

    :memo: parse()用のテスト

--- common/src/commonTest/kotlin/com/uramnoil/ansies/AnsiesTest.kt
+++ common/src/commonTest/kotlin/com/uramnoil/ansies/AnsiesTest.kt
@@ -5,101 +5,97 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class AnsiesTest {
-    companion object {
-        fun csiWithReset(arg: String, string: String = "") = "${Char(0x1B)}[${arg}m${string}${reset.build()}"
-        fun csi(arg: String, string: String) = "${Char(0x1B)}[${arg}m${string}"
-    }
-
     @Test
     fun testSgrOfBlack() {
         assertEquals(
-            csiWithReset("30"),
-             (sgrOf(SetForegroundColorBlack) + reset).build()
+            sgrWithReset("3"),
+            (ansiOf(BlackForeground) + reset).build()
         )
     }
 
     @Test
     fun testSgrOfRed() {
         assertEquals(
-            csiWithReset("31"),
-             (sgrOf(SetForegroundColorRed) + reset).build()
+            sgrWithReset("31"),
+            (ansiOf(RedForeground) + reset).build()
         )
     }
 
     @Test
     fun testSgrOfGreen() {
         assertEquals(
-            csiWithReset("32"),
-             (sgrOf(SetForegroundColorGreen) + reset).build()
+            sgrWithReset("32"),
+            (ansiOf(GreenForeground) + reset).build()
         )
     }
 
     @Test
     fun testSgrOfYellow() {
         assertEquals(
-            csiWithReset("33"),
-             (sgrOf(SetForegroundColorYellow) + reset).build()
+            sgrWithReset("33"),
+            (ansiOf(YellowForeground) + reset).build()
         )
     }
 
     @Test
     fun testSgrOfBlue() {
         assertEquals(
-            csiWithReset("34"),
-             (sgrOf(SetForegroundColorBlue) + reset).build()
+            sgrWithReset("34"),
+            (ansiOf(BlueForeground) + reset).build()
         )
     }
 
     @Test
     fun testSgrOfMagenta() {
         assertEquals(
-            csiWithReset("35"),
-             (sgrOf(SetForegroundColorMagenta) + reset).build()
+            sgrWithReset("35"),
+            (ansiOf(MagentaForeground) + reset).build()
         )
     }
 
     @Test
     fun testSgrOfCyan() {
         assertEquals(
-            csiWithReset("36"),
-             (sgrOf(SetForegroundColorCyan) + reset).build()
+            sgrWithReset("36"),
+            (ansiOf(CyanForeground) + reset).build()
         )
     }
 
     @Test
     fun testSgrOfWhite() {
         assertEquals(
-            csiWithReset("37"),
-             (sgrOf(SetForegroundColorWhite) + reset).build()
+            sgrWithReset("37"),
+            (ansiOf(WhiteForeground) + reset).build()
         )
     }
 
     @Test
-    fun testSgrOfColorPalette() {
+    fun testSgrOfPalette() {
         assertEquals(
-            csiWithReset("38;5;0"),
-            (sgrOf(SetForegroundColorWith(0u)) + reset).build()
+            sgrWithReset("38;5;0"),
+            (ansiOf(SelectForegroundColor(0u)) + reset).build()
         )
     }
 
     @Test
     fun testSgrOfRgb() {
         assertEquals(
-            csiWithReset("38;2;0;100;200"),
-            (sgrOf(SetForegroundColorWith(0u, 100u, 200u)) + reset).build()
+            sgrWithReset("38;2;0;100;200"),
+            (ansiOf(SelectForegroundColorRgb(0u, 100u, 200u)) + reset).build()
         )
     }
 
     @Test
     fun testSgrOfAnsiEscapeBuilderPlus() {
         assertEquals(
-            csi("30", "hoge") + csiWithReset("37", "fuga"),
-            (sgrOf(SetForegroundColorBlack) + "hoge" + sgrOf(SetForegroundColorWhite) + "fuga" + reset).build()
+            sgr("30") + "hoge" + sgrWithReset("37", "fuga"),
+            (ansiOf(BlackBackground) + "hoge" + ansiOf(WhiteForeground) + "fuga" + reset).build()
         )
     }
 
     @Test
     fun testSgrOfMultipleSgr() {
+
     }
 
     @Test
