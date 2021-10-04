2374efe8d936d5a593842f9aa5bf02005eb40e5d
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-10-04 22:37:45 +0900

    :sparkles: submitIfInputIsNotBlank

--- desktop/src/main/kotlin/com/uramnoil/awesome_minecraft_console/endervision/compose/molecules/CommandController.kt
+++ desktop/src/main/kotlin/com/uramnoil/awesome_minecraft_console/endervision/compose/molecules/CommandController.kt
@@ -1,28 +1,46 @@
 package com.uramnoil.awesome_minecraft_console.endervision.compose.molecules
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onKeyEvent
 import com.uramnoil.awesome_minecraft_console.endervision.compose.atoms.CommandInput
 import com.uramnoil.awesome_minecraft_console.endervision.compose.atoms.SubmitButton
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun CommandController(onSubmitCommand: (String) -> Unit = {}) {
     var command by remember { mutableStateOf("") }
 
-    BoxWithConstraints(Modifier.fillMaxSize()) {
+    val submitIfInputIsNotBlank = {
+        if (command.isNotBlank()) {
+            onSubmitCommand(command)
+            command = ""
+        }
+    }
+
+    BoxWithConstraints(Modifier.fillMaxSize()
+        .onKeyEvent {
+            if (it.key.keyCode == Key.Enter.keyCode) {
+                submitIfInputIsNotBlank()
+                false
+            } else {
+                true
+            }
+        }
+    ) {
         Row {
             Box(Modifier.weight(90f)) {
                 CommandInput(command, onValueChange = { command = it })
             }
             Box(Modifier.weight(10f)) {
-                SubmitButton {
-                    onSubmitCommand(command)
-                    command = ""
-                }
+                SubmitButton { submitIfInputIsNotBlank() }
             }
         }
     }
