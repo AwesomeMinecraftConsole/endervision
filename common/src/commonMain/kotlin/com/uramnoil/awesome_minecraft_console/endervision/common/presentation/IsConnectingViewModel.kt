b2c295b67f03cb18e28d26006a55c0021d22b193
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2021-10-09 13:46:47 +0900

    :sparkles: IsConnectingViewModel

---
+++ common/src/commonMain/kotlin/com/uramnoil/awesome_minecraft_console/endervision/common/presentation/IsConnectingViewModel.kt
@@ -0,0 +1,5 @@
+package com.uramnoil.awesome_minecraft_console.endervision.common.presentation
+
+import kotlinx.coroutines.flow.StateFlow
+
+class IsConnectingViewModel(val isConnecting: StateFlow<Boolean>)
